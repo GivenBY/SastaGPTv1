ea0136f8997ea7d13234018c53bdca8470197a4a
GivenBY <suryakantupadhyay18@gmail.com>
2023-10-04 21:15:51 +0530

    feat: Implement dynamic chat functionality
    Refactor JavaScript code to dynamically handle user interactions with the chat interface. Add event listeners to handle button clicks and API requests. Create functions to generate chat elements dynamically based on user input and API responses. Implement error handling and user input validation. Add appropriate comments to enhance code readability.
    
    Changes made:
    - Added event listeners for button clicks and user input handling.
    - Created functions to dynamically generate chat elements for questions and answers.
    - Implemented API interactions for fetching responses.
    - Added error handling and input validation logic.
    - Included detailed comments for code clarity.
    
    This commit enhances the chat application's functionality, providing a more interactive and user-friendly experience.

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
-from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
-import openai
 import os
+from pymongo import MongoClient
+from flask import Flask, jsonify, request, render_template
+from dotenv import load_dotenv
+import openai
+
+# Load environment variables from .env file
+load_dotenv()
+
 app = Flask(__name__)
 
-load_dotenv()
-openai.api_key = os.environ['api_key']
+mongo_url = os.getenv('MONGO_URL')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+if not mongo_url or not openai.api_key:
+    raise ValueError("Invalid or missing environment variables.")
+
+mongo = MongoClient(mongo_url)
+db = mongo.get_database("sastagpt")
 
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    chats = mongo.db.chats.find({})
+    myChats = [chat for chat in chats]
+    print(myChats)
+    return render_template("index.html", myChats=myChats)
 
 
 @app.route("/api", methods=["GET", "POST"])
 def qa():
     if request.method == "POST":
         question = request.json.get("question")
-        # chat = mongo.db.chats.find_one({"question": question})
-        # print(chat)
-        # if chat:
-        #     data = {"question": question, "answer": f"{chat['answer']}"}
-        #     return jsonify(data)
-        if (question == "What is your name?"):
-            print("Name")
+        chat = mongo.db.chats.find_one({"question": question})
+        print(chat)
+        if chat:
+            data = {"question": question, "answer": f"{chat['answer']}"}
+            print(data)
+            return jsonify(data)
         else:
             completion = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -33,8 +46,8 @@ def qa():
 
             data = {"question": question,
                     "answer": completion['choices'][0]['message']['content']}
-            # mongo.db.chats.insert_one(
-            #     {"question": question, "answer": response["choices"][0]["text"]})
+            mongo.db.chats.insert_one(
+                {"question": question, "answer": completion['choices'][0]['message']['content']})
             return jsonify(data)
     data = {"result": "Thank you! I'm just a machine learning model designed to respond to questions and generate text based on my training data. Is there anything specific you'd like to ask or discuss? "}
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,4 @@
+// Function to send a POST request to the specified URL with provided data
 async function postData(url = "", data = {}) {
   const response = await fetch(url, {
     method: "POST",
@@ -8,19 +9,86 @@ async function postData(url = "", data = {}) {
   });
   return response.json();
 }
+
+// Function to encode HTML characters in a string
 function htmlEncode(str) {
   return String(str).replace(/[^\w. ]/gi, function (c) {
     return "&#" + c.charCodeAt(0) + ";";
   });
 }
 
+// Event listener for the button click
 sendButton__button.addEventListener("click", async () => {
-  document.querySelector(".question").style.display = "flex";
-  document.querySelector(".answer").style.display = "flex";
+  // Get the user's input from the text area
+  let questionValues = document.getElementById("askq").value;
 
+  // Create new elements for the question and answer
+  const newQuestionElement = document.createElement("div");
+  newQuestionElement.className = "question";
+  newQuestionElement.style.display = "flex";
+  const newAnswerElement = document.createElement("div");
+  newAnswerElement.className = "answer";
+  newAnswerElement.style.display = "flex";
+
+  // Create element for displaying the user's question
+  const questionTextElement = document.createElement("div");
+  questionTextElement.className = "questionText";
+  questionTextElement.style.width = "60%";
+  questionTextElement.textContent = questionValues;
+  newQuestionElement.appendChild(questionTextElement);
+
+  // Create element for displaying "Loading..." while waiting for the API response
+  const answerTextElement = document.createElement("pre");
+  answerTextElement.className = "answerText";
+  answerTextElement.textContent = "Loading...";
+  newAnswerElement.appendChild(answerTextElement);
+
+  // Append question and loading message to the chat content container
+  document.querySelector(".chatContent").appendChild(newQuestionElement);
+  document.querySelector(".chatContent").appendChild(newAnswerElement);
+
+  // Make a POST request to the server API with the user's question
+  let result = await postData("/api", { question: questionValues });
+
+  // Populate the answer element with the response from the API
+  answerTextElement.textContent = result.answer;
+
+  // Get the chat history container
+  const chatHistoryContainer = document.querySelector(".chats");
+
+  // Create a new chat element with the user's question
   let questionValue = document.getElementById("askq").value;
-  const answerElement = document.querySelector(".questionText");
-  answerElement.textContent = questionValue;
-  let result = await postData("/api", { question: questionValue });
-  document.querySelector(".answerText").textContent = result.answer;
+  const questionElement = document.createElement("div");
+  questionElement.className = "chat-list";
+
+  // Create an icon for the chat element
+  const icon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  icon.setAttribute("stroke", "currentColor");
+  icon.setAttribute("fill", "none");
+  icon.setAttribute("stroke-width", "2");
+  icon.setAttribute("viewBox", "0 0 24 24");
+  icon.setAttribute("stroke-linecap", "round");
+  icon.setAttribute("stroke-linejoin", "round");
+  icon.setAttribute("class", "icon-sm");
+  icon.setAttribute("height", "1em");
+  icon.setAttribute("width", "1em");
+
+  const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute(
+    "d",
+    "M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"
+  );
+
+  icon.appendChild(path);
+
+  // Create text node with the user's question
+  const questionText = document.createTextNode(questionValue);
+  questionElement.appendChild(icon);
+  questionElement.appendChild(questionText);
+
+  // Append the user's question to the chat history container
+  chatHistoryContainer.appendChild(questionElement);
+
+  // Clear the input field after sending the question
+  document.getElementById("askq").value = "";
 });
